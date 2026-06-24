28c6eb0731f22c3a9514e44b7eae6cc38806cf69
yutaro <kun195342@gmail.com>
2026-06-24 17:52:13 +0900

    I

--- chat.go
+++ chat.go
@@ -16,6 +16,14 @@ import (
 // handleCreateRoom チャット部屋の作成・または既存の部屋を返す (POST /api/rooms)
 func handleCreateRoom(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		if r.Method != http.MethodPost {
 			respondWithError(w, http.StatusMethodNotAllowed, "Method Not Allowed")
 			return
